ce1aabd275f068e73fedb9b41a8faff89f3eb646
Dan_Nazan <danzan@list.ru>
2021-04-17 11:39:58 +0300

    begin

---
+++ tutorial/tutorapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name = "home"),
+    path("about/", views.about, name = "about"),
+    path("contacts/", views.contacts, name = "contacts"),
+    path("digest/", views.digest, name = "digest"),
+    path("promo/", views.promo, name = "promo"),
+    path("tutors/", views.about, name = "tutors"),
+    path("vacancies/", views.vacancies, name = "vacancies"),
+]
---
+++ tutorial/tutorapp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils.timezone import datetime
+
+
+def home(request):
+    return render(request, "tutorapp/base.html")
+
+def about(request):
+    return render(request, 'tutorapp/about.html', {'name': name, 'date': datetime.now()})
+def contacts(request):
+    return render(request, "tutorapp/contacts.html")
+
+def digest(request):
+    return render(request, "tutorapp/digest.html")
+def promo(request):
+    return render(request, "tutorapp/promo.html")
+
+def tutors(request):
+    return render(request, "tutorapp/tutors.html")
+
+def vacancies(request):
+    return render(request, "tutorapp/vacancies.html")
+
+
+
+# Create your views here.
